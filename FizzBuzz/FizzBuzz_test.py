363d7eb9a10af2e71f830004b8d45e1f7617cb44
Kyle Galloway <kyle.galloway.1993@gmail.com>
2018-06-18 12:31:32 -0500

    Split FizzBuzz & tests

--- FizzBuzz/FizzBuzz_test.py
+++ FizzBuzz/FizzBuzz_test.py
@@ -1,18 +1,6 @@
 import pytest
 
-
-def fizzBuzz(value):
-    if isMultiple(value, 3):
-        if isMultiple(value, 5):
-            return "FizzBuzz"
-        return "Fizz"
-    if isMultiple(value, 5):
-        return "Buzz"
-    return str(value)
-
-
-def isMultiple(value, mod):
-    return (value % mod) == 0
+from FizzBuzz import fizzBuzz
 
 
 def checkFizzBuzz(value, expectedRetVal):
